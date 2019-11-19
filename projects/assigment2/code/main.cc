fba926deddc43d8db8e7b683a8b98c37a82483a9
Blinningjr <nicke.l@telia.com>
2019-11-19 13:30:42 +0100

    Started on lab2

---
+++ projects/assigment2/code/main.cc
@@ -0,0 +1,15 @@
+//------------------------------------------------------------------------------
+// main.cc
+// (C) 2015-2017 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#include "config.h"
+
+int main(int argc, const char** argv) {
+	// Snowflake::SnowflakeApp app;
+	// if (app.Open())
+	// {
+	// 	app.Run();
+	// 	app.Close();
+	// }
+	// app.Exit();
+}
